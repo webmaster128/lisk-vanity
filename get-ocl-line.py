005abdb0f4ac264cf0d6354048c61a0565f30148
Simon Warta <simon@kullo.net>
2018-12-17 23:45:36 +0100

    Let python script print all the kernel lines

--- get-ocl-line.py
+++ get-ocl-line.py
@@ -1,24 +1,22 @@
-#!/usr/bin/env python
-
-# Gets the actual file and line number from an OpenCL error line number
+#!/usr/bin/env python3
 
 import sys
 
 files = [
-    "blake2b.cl",
+    "types.cl",
     "curve25519-constants.cl",
     "curve25519-constants2.cl",
     "curve25519.cl",
-    "entry.cl"
+    "sha/inc_hash_functions.cl",
+    "sha/sha256.cl",
+    "sha/sha512.cl",
+    "sha_bindings.cl",
+    "bip39.cl",
+    "lisk.cl",
+    "entry.cl",
 ]
 
-num = int(sys.argv[1])
-
 for f_name in files:
-    f = open("src/opencl/" + f_name, "r")
-    lines = len(f.readlines())
-    lines += 1
-    if num > lines:
-        num -= lines
-    else:
-        print(f_name + ":" + str(num))
+    with open("src/opencl/" + f_name, "r") as f:
+        for line in f:
+            sys.stdout.write(line)
